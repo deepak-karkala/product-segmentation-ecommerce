a5b07d5e20c1f65c0df2dc5ebd11a78ac3772b3e
deepak-karkala <dkarkala01@gmail.com>
2020-11-21 16:59:27 +0530

    Model returns segmented product, position and size of image can be adjusted

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -3,21 +3,26 @@ import pickle
 from flask import Flask
 import flask
 #from flask_bootstrap import Bootstrap
-import sklearn
 import joblib
+import numpy as np
 import pandas as pd
 from webapp.db import get_db
+import tensorflow as tf
+from tensorflow import keras
+from PIL import Image
+
 
 # Load pre-trained machine learning model.
-base_path = "/Users/nesara/Documents/aim/cs/projects/airbnb-data-science/webapp_alternate_search/";
-#with open(base_path + 'static/model/decision_tree.pkl', 'rb') as f:
-#    model = pickle.load(f)
-#model = joblib.load(base_path + "static/model/fullpipeline_linearregression.pkl")
+base_path = "/Users/nesara/Documents/aim/cs/projects/product-segmentation-ecommerce/webapp/static/";
+load_model_path = base_path + "models/model.h5"
+#from keras.models import load_model
+model = keras.models.load_model(load_model_path)
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
@@ -53,7 +58,9 @@ def create_app(test_config=None):
             #all_images = os.listdir(os.path.join(app.static_folder, "images"))
             selected_image = None
             filter_product_category = None
-            return(flask.render_template('base.html', images=all_images, selected_image=selected_image, filter_product_category=filter_product_category))
+            product_segmented_image_path = None
+            return(flask.render_template('base.html', images=all_images, selected_image=selected_image,
+                filter_product_category=filter_product_category, product_segmented_image_path=product_segmented_image_path))
 
 
         # Return prediction output
@@ -68,9 +75,17 @@ def create_app(test_config=None):
                 selected_image = db.execute(
                         'SELECT id, category, image_path FROM product p WHERE p.id = ' + selected_image_id 
                     ).fetchone()
-                print(selected_image)
+
+                # Get segmentation mask from model (Run inference on model)
+                full_image_path = base_path + "images/" + selected_image["image_path"]
+                product_segmented_image_path = get_model_output_segmentation_mask(full_image_path)
+                product_segmented_image_path = "product_segmented_image/" + "test.png"
+                # product_segmented_image_path = None
+                # <img id="overlay_image" src="{{url_for('static', filename=product_segmented_image_path)}}" />
+                
             else:
                 selected_image = None
+                product_segmented_image_path = None
             
             if ("filter_product_button" in flask.request.form) and (flask.request.form["filter_product_button"] != "All"):
                 filter_product_category = flask.request.form["filter_product_button"]
@@ -96,9 +111,7 @@ def create_app(test_config=None):
             #all_images = os.listdir(os.path.join(app.static_folder, "images"))
             #return(flask.render_template('base.html', images=all_images))
             return(flask.render_template('base.html', images=all_images, selected_image=selected_image,
-                filter_product_category=filter_product_category))
-
-
+                filter_product_category=filter_product_category, product_segmented_image_path=product_segmented_image_path))
 
     #Bootstrap(app)
 
@@ -106,3 +119,66 @@ def create_app(test_config=None):
     db.init_app(app)
 
     return app
+
+
+
+
+def get_model_output_segmentation_mask(image_path):
+    """
+    Run inference, return segmented product
+    """
+    # Preprocess image
+    input_image = preprocess_image(image_path)
+    
+    # Add extra dimension for model input
+    input_image_model = np.expand_dims(input_image, axis=0)
+    
+    # Run inference
+    pred_mask = model.predict(input_image_model)
+    
+    # Select category with highest activation
+    seg_mask = tf.argmax(pred_mask, axis=-1)
+    seg_mask = seg_mask[..., tf.newaxis]
+    seg_mask = seg_mask[0]
+
+    # Set transparency to background pixels detected in mask
+    product_segmented = get_segmented_product(input_image, seg_mask)
+
+    # Save segmented product as image
+    product_segmented_image_path = "product_segmented_image/" + "product_segmented_image.png"
+    product_segmented.save(base_path + product_segmented_image_path, "PNG")
+
+    return product_segmented_image_path
+
+
+def preprocess_image(image_path):
+    # Load image as array
+    raw_image = tf.keras.preprocessing.image.load_img(image_path)
+    arr_image = tf.keras.preprocessing.image.img_to_array(raw_image)
+    # Resize
+    input_image = tf.image.resize(arr_image, (128, 128))
+    # Normalise
+    input_image = tf.cast(input_image, tf.float32) / 255.0
+
+    # Add extra dimension for model input
+    # input_image = np.expand_dims(input_image, axis=0)
+
+    return input_image
+
+def get_segmented_product(input_image, seg_mask):
+    img = tf.keras.preprocessing.image.array_to_img(input_image)
+    img = img.convert("RGBA")
+    imgpixdata = img.load()
+
+    msk = tf.keras.preprocessing.image.array_to_img(seg_mask)
+    msk = msk.convert("RGBA")
+    mskpixdata = msk.load()
+
+    # Set transparency to background pixels detected in mask
+    width, height = img.size
+    for y in range(height):
+        for x in range(width):
+            if mskpixdata[x, y] == (0, 0, 0, 255):
+                imgpixdata[x, y] = (0, 0, 0, 0)
+
+    return img
--- webapp/static/webcam_stream.js
+++ webapp/static/webcam_stream.js
@@ -11,11 +11,36 @@ if (navigator.mediaDevices.getUserMedia) {
     });
 }
 
+var tooltip = d3.select("body")
+        .append("div")
+        .attr("class", "tooltip1")
+        .style("visibility", "hidden");
 
 
 // http://jsfiddle.net/2kev63mq/1/
-$("#show_result").click(function (e) {
+$("#videoElement").click(function (e) {
 
+	var videoWidth = d3.select("#videoElement").node().offsetWidth;
+	var videoHeight = d3.select("#videoElement").node().offsetHeight;
+	var videoLeft = $("#videoElement").offset().left;
+	var videoTop = $("#videoElement").offset().top;
+	var videoBottom = videoTop + videoHeight;
+	var videoRight = videoLeft + videoWidth;
+	var videoCenterOffset = videoLeft + videoWidth*0.50;
+
+	if (e.pageX >= videoCenterOffset) {
+		return tooltip.html(`<img src='static/product_segmented_image/test.png'>`)
+				.style("top", Math.min(videoBottom-150, (e.pageY)) + "px")
+				.style("left", Math.max(videoLeft, (e.pageX-300)) + "px")
+				.style("visibility", "visible");
+	} else {
+		return tooltip.html(`<img src='static/product_segmented_image/test.png'>`)
+				.style("top", Math.min(videoBottom-150,(e.pageY)) + "px")
+				.style("left", Math.max(videoLeft, (e.pageX-50)) + "px")
+				.style("visibility", "visible");
+	}
+
+/*
     //height and width of the container
     //var height = $("#pcVideo").height();
     //var width = $("#pcVideo").width();
@@ -24,13 +49,39 @@ $("#show_result").click(function (e) {
     var relativeX = e.pageX - this.offsetLeft;
     var relativeY = e.pageY - this.offsetTop;
 
+    console.log(e.pageX);
+
+
     //$('#overlay_image').css("left", relativeX - 25);
     //$('#overlay_image').css("top", relativeY - 25);
-    $('#overlay_image').css("left", e.pageX);
-    $('#overlay_image').css("top", e.pageY);
+    $('#overlay_image').css("left", (e.pageX));
+    $('#overlay_image').css("top", (e.pageY));
+    //$('#overlay_image').css("display", "flex");
     $('#overlay_image').show();
     //overlay
     var video = document.getElementById('videoElement');
 
     //video.pause();
+*/
 });
+
+
+set_size_slider();
+function set_size_slider() {
+	//var handle = $( "#custom-handle" );
+	$( "#slider-range-min" ).slider({
+	  create: function() {
+	    //handle.text( $( this ).slider( "value" ) );
+	  },
+	  slide: function( event, ui ) {
+	  },
+	  change: function(event, ui) {
+	   	tooltip.style("width", ui.value+"vh").style("height", "auto");
+	  },
+	  range: "min",
+	  max: 40,
+	  min: 10,
+	  step: 5,
+	  value: 30,
+	});
+}
