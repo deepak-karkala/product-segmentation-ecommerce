c265e5f7fd3aff8a76cb3ef8d2a4ec48b1a0f011
deepak-karkala <dkarkala01@gmail.com>
2020-11-30 10:22:21 +0530

    Updated with REST API to get model prediction

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,4 +1,9 @@
+import sys
+sys.path.append("/Users/nesara/Documents/aim/cs/projects/product-segmentation-ecommerce/")
+import webapp
+
 import os
+import io
 import pickle
 from flask import Flask
 import flask
@@ -6,18 +11,22 @@ import flask
 import joblib
 import numpy as np
 import pandas as pd
-from webapp.db import get_db
+from webapp.db import get_db, init_app
 import tensorflow as tf
 from tensorflow import keras
 from PIL import Image
 
 
-# Load pre-trained machine learning model.
 base_path = "/Users/nesara/Documents/aim/cs/projects/product-segmentation-ecommerce/webapp/static/";
-load_model_path = base_path + "models/model.h5"
-#from keras.models import load_model
-model = keras.models.load_model(load_model_path)
 
+def load_model():
+    # Load pre-trained machine learning model.
+    load_model_path = base_path + "models/model.h5"
+    #from keras.models import load_model
+    model = keras.models.load_model(load_model_path)
+    return model
+
+model = load_model()
 
 
 def create_app(test_config=None):
@@ -113,14 +122,65 @@ def create_app(test_config=None):
             return(flask.render_template('base.html', images=all_images, selected_image=selected_image,
                 filter_product_category=filter_product_category, product_segmented_image_path=product_segmented_image_path))
 
-    #Bootstrap(app)
 
-    from . import db
-    db.init_app(app)
+    # Predict and return JSON data
+    @app.route("/predict", methods=["POST"])
+    def predict():
+        # initialize the data dictionary that will be returned from the view
+        data = {"success": False}
+
+        # ensure an image was properly uploaded to our endpoint
+        if flask.request.method == "POST":
+            if flask.request.files.get("image"):
+                data["predictions"] = []
+
+                # Read input as image
+                image = flask.request.files["image"].read()
+                image = Image.open(io.BytesIO(image))
+
+                # Run inference and get prediction
+                seg_mask, seg_product_category = get_model_output_json(image)
+
+                # Add prediction results to JSON data
+                r = {"label": seg_product_category} #, "mask": np.array(seg_mask).tolist()}
+                data["predictions"].append(r)
+
+                # indicate that the request was a success
+                data["success"] = True
+
+        # return the data dictionary as a JSON response
+        return flask.jsonify(data)
+
+
+    #Bootstrap(app)
+    init_app(app)
 
     return app
 
 
+def get_model_output_json(image):
+    """
+    Run inference, return JSON output
+    """
+    # Preprocess image
+    input_image = preprocess_image(image)
+    
+    # Add extra dimension for model input
+    input_image_model = np.expand_dims(input_image, axis=0)
+    
+    # Run inference
+    pred_mask = model.predict(input_image_model)
+    
+    # Select category with highest activation
+    seg_mask = tf.argmax(pred_mask, axis=-1)
+    seg_mask = seg_mask[..., tf.newaxis]
+    seg_mask = seg_mask[0]
+
+    # Get product category from mask content
+    LABEL_NAMES = ['background', 'couch', 'chair', 'bed', 'vase', 'bowl', 'cup', 'wine-glass', 'potted-plant']
+    seg_product_category = LABEL_NAMES[np.argmax(np.array(tf.reduce_sum(tf.reduce_sum(pred_mask, 1),1))[0][1:])+1]
+
+    return seg_mask, seg_product_category
 
 
 def get_model_output_segmentation_mask(image_path):
@@ -128,7 +188,7 @@ def get_model_output_segmentation_mask(image_path):
     Run inference, return segmented product
     """
     # Preprocess image
-    input_image = preprocess_image(image_path)
+    input_image = preprocess_image_path(image_path)
     
     # Add extra dimension for model input
     input_image_model = np.expand_dims(input_image, axis=0)
@@ -151,7 +211,7 @@ def get_model_output_segmentation_mask(image_path):
     return product_segmented_image_path
 
 
-def preprocess_image(image_path):
+def preprocess_image_path(image_path):
     # Load image as array
     raw_image = tf.keras.preprocessing.image.load_img(image_path)
     arr_image = tf.keras.preprocessing.image.img_to_array(raw_image)
@@ -164,6 +224,22 @@ def preprocess_image(image_path):
     # input_image = np.expand_dims(input_image, axis=0)
 
     return input_image
+
+
+def preprocess_image(image):
+    # Load image as array
+    #raw_image = tf.keras.preprocessing.image.load_img(image_path)
+    arr_image = tf.keras.preprocessing.image.img_to_array(image)
+    # Resize
+    input_image = tf.image.resize(arr_image, (128, 128))
+    # Normalise
+    input_image = tf.cast(input_image, tf.float32) / 255.0
+
+    # Add extra dimension for model input
+    # input_image = np.expand_dims(input_image, axis=0)
+
+    return input_image
+
 
 def get_segmented_product(input_image, seg_mask):
     img = tf.keras.preprocessing.image.array_to_img(input_image)
@@ -182,3 +258,13 @@ def get_segmented_product(input_image, seg_mask):
                 imgpixdata[x, y] = (0, 0, 0, 0)
 
     return img
+
+
+# if this is the main thread of execution first load the model and
+# then start the server
+if __name__ == "__main__":
+    print(("* Loading Keras model and Flask starting server..."
+        "please wait until server has fully started"))
+    # model = load_model(base_path)
+    app = create_app()
+    app.run(host='0.0.0.0')
